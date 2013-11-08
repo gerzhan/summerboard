9587f5b6859e737d7c9c0d301c49bae0fe6fc718
Alan Hong <susukang98@gmail.com>
2013-11-08 13:29:50 +0900

    Extract module with auth.js

--- app.js
+++ app.js
@@ -9,74 +9,9 @@ var user = require('./routes/user');
 var board = require('./routes/board');
 var http = require('http');
 var path = require('path');
-var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+var auth = require('./middlewares/auth');
+var passport = require('passport');
 var flash = require('connect-flash');
-
-var users = [
-    { id: 1, username: 'alan', password: '1234', email: 'alan@kakao.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-
-// Use the LocalStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.  In the real world, this would query a database;
-//   however, in this example we are using a baked-in set of users.
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      })
-    });
-  }
-));
 
 var app = express();
 
@@ -91,8 +26,8 @@ app.use(express.methodOverride());
 app.use(express.cookieParser());
 app.use(express.session({secret: "Super simple Task board"}));
 app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(auth.initialize());
+app.use(auth.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -122,7 +57,7 @@ app.post('/login', passport.authenticate('local', { failureRedirect: '/login', f
 //   login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  res.redirect('/')
 }
 
 app.get('/logout', function(req, res) {
@@ -132,7 +67,7 @@ app.get('/logout', function(req, res) {
 
 // pages
 app.get('/', routes.index);
-app.get('/boards', board.index);
+app.get('/boards', ensureAuthenticated, board.index);
 
 // boards
 app.get('/cards', board.listCard);
